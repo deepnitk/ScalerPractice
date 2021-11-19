abea347415d563c15dcb69149c277326ffe13f49
ratnadeep konar <ratnadeep.konar@gmail.com>
2021-11-19 13:11:14 +0530

    Update PermutationOfAB.java

--- PermutationOfAB.java
+++ PermutationOfAB.java
@@ -8,9 +8,6 @@ public class Solution {
             char c = A.charAt(i);
             freq.set(c-'a', freq.get(c-'a')+1);
         }
-        // freq.stream().forEach((a)->{
-        //     System.out.print(a);
-        // });
         freqA.put(freq, 1);
 
         ArrayList<Integer> freqSubString = new ArrayList<Integer>(Collections.nCopies(26, 0));
@@ -19,10 +16,6 @@ public class Solution {
             char c = B.charAt(i);
             freqSubString.set(c-'a', freqSubString.get(c-'a')+1);
         }
-        // System.out.println();
-        // freqSubString.stream().forEach((a)->{
-        //     System.out.print(a);
-        // });
 
         
         if(freqA.containsKey(freqSubString)) count++;
@@ -30,10 +23,8 @@ public class Solution {
 
         for(int j = i; j<B.length();j++){
             char c1 = B.charAt(j);
-            // System.out.println("c1"+c1);
             freqSubString.set(c1-'a',freqSubString.get(c1-'a')+1);
             char c2 = B.charAt(j-A.length());
-            // System.out.println("c2"+c2);
             freqSubString.set(c2-'a', freqSubString.get(c2-'a') == 0 ? 0 : freqSubString.get(c2-'a')-1);
             if(freqA.containsKey(freqSubString)) count++;
         }
