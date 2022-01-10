95b9a834b14789c014a5268dad1b4a59dbeff739
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-01-10 15:43:03 +0530

    Create ReverseString_rec.java
    Write a recursive function that, given a string S, prints the characters of S in reverse order.

---
+++ ReverseString_rec.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // YOUR CODE GOES HERE
+        // Please take input and print output to standard input/output (stdin/stdout)
+        // DO NOT USE ARGUMENTS FOR INPUTS
+        // E.g. 'Scanner' for input & 'System.out' for output
+        Scanner sc=new Scanner(System.in);  
+        String str = sc.nextLine();
+        reverse(str);
+    }
+    public static void reverse(String str)
+    {
+        if ((str==null)||(str.length() <= 1))
+           System.out.println(str);
+        else
+        {
+            System.out.print(str.charAt(str.length()-1));
+            reverse(str.substring(0,str.length()-1));
+        }
+    }
+
+}
