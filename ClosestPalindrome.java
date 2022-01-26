c8e3a0272e616724db063d230ad3e9d2e2ce83ff
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-01-26 19:09:27 +0530

    Update ClosestPalindrome.java
    Groot has a profound love for palindrome which is why he keeps fooling around with strings.
    A palindrome string is one that reads the same backward as well as forward.
    
    Given a string A of size N consisting of lowercase alphabets, he wants to know if it is possible to make the given string a palindrome by changing exactly one of its character.

--- ClosestPalindrome.java
+++ ClosestPalindrome.java
@@ -1,8 +1,3 @@
-// Groot has a profound love for palindrome which is why he keeps fooling around with strings.
-// A palindrome string is one that reads the same backward as well as forward.
-
-// Given a string A of size N consisting of lowercase alphabets, he wants to know if it is possible to make the given string a palindrome by changing exactly one of its character.
-
 public class Solution {
     public String solve(String A) {
         int diffCount = 0;
