1d58d98b668c826c0eb4d86673f5e28e48ad781b
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-01-29 14:04:22 +0530

    Create LinkedList.java
    Design and implement a Linked List data structure. A node in a linked list should have the following attributes - an integer value and a pointer to the next node. It should support the following operations:
    
    insert_node(position, value) - To insert the input value at the given position in the linked list.
    delete_node(position) - Delete the value at the given position from the linked list.
    print_ll() - Print the entire linked list, such that each element is followed by a single space.
    Note:
    
    If an input position does not satisfy the constraint, no action is required.
    Each print query has to be executed in new line.

---
+++ LinkedList.java
@@ -0,0 +1,65 @@
+
+    static class node {
+        int data;
+        node next;
+        public node(){
+            this.data =0;
+            this.next = null;
+        }
+    }
+
+    static node root = null;
+    static int size_of_ll = 0;
+
+    public static void insert_node(int position, int value) {
+        node temp = new node();
+        temp.data = value;
+        if(position ==1){
+            temp.next= root;
+            root= temp;
+        } else {
+            int count=1;
+            node prev = root;
+            while (count < position - 1) {
+                    prev = prev.next;
+                    count++;
+            }
+            temp.next= prev.next;
+            prev.next= temp;
+        }
+        size_of_ll++;    
+    }
+
+    public static void delete_node(int position) {
+        if(position>=1 && position<=size_of_ll){
+            
+            node temp = null;
+            if (position == 1) {
+                temp = root;
+                root = root.next;
+            } else {
+                int count = 1;
+                node prev = root;
+                while (count < position - 1) {
+                    prev = prev.next;
+                    count++;
+                }
+                temp = prev.next;
+                prev.next = prev.next.next;
+            }
+            size_of_ll--;
+        }
+    }
+
+    public static void print_ll() {
+        int flag = 0;
+        node temp=root;
+        while(temp!=null){
+            if (flag == 0) {
+                System.out.print(temp.data);
+                flag = 1;
+            } else
+                System.out.print(" " + temp.data);
+            temp=temp.next;
+        }
+    }
