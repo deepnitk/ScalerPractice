da67196ea0ff645ad4764a33b97db2d35a820d46
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-01-29 10:33:11 +0530

    Create DeleteMiddleInLinkedList.java
    Given a singly linked list, delete middle of the linked list.
    
    For example, if given linked list is 1->2->3->4->5 then linked list should be modified to 1->2->4->5
    
    If there are even nodes, then there would be two middle nodes, we need to delete the second middle element.
    
    For example, if given linked list is 1->2->3->4->5->6 then it should be modified to 1->2->3->5->6.
    
    Return the head of the linked list after removing the middle node.
    
    If the input linked list has 1 node, then this node should be deleted and a null node should be returned.

---
+++ DeleteMiddleInLinkedList.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     ListNode(int x) { val = x; next = null; }
+ * }
+ */
+public class Solution {
+    public ListNode solve(ListNode head) {
+         if(head== null)
+            return null;
+        ListNode temp=head;
+        int noOfNodes = 0;
+        while(temp !=null){
+            temp=temp.next;
+            noOfNodes++;
+        }
+        int k= (noOfNodes/2)+1;
+        ListNode prev=null;
+        ListNode nxt =null;
+        ListNode curr=head;
+        while(curr.next!=null && k>1){
+            prev=curr;
+            curr= curr.next;
+            nxt=curr.next;
+            k--;
+        }
+        prev.next= nxt;
+        return head;
+
+
+    }
+}
