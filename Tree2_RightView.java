e6fc7f6ac59ceea4ace7a7085181305817fb4253
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-02-12 17:33:50 +0530

    Create Tree2_RightView.java
    Given a binary tree of integers denoted by root A. Return an array of integers representing the right view of the Binary tree.
    
    Right view of a Binary Tree is a set of nodes visible when the tree is visited from Right side.

---
+++ Tree2_RightView.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for binary tree
+ * class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) {
+ *      val = x;
+ *      left=null;
+ *      right=null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ArrayList<Integer> solve(TreeNode A) {
+        ArrayList<Integer> rightView = new ArrayList<>();
+        if(A==null)
+            return rightView;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(A);
+        while(!q.isEmpty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode node = q.remove();
+                if(i==size-1) rightView.add(node.val);
+                if(node.left!=null) q.add(node.left);
+                if(node.right!=null) q.add(node.right);
+            }
+        }
+        return rightView;
+
+
+    }
+}
