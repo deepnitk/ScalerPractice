38c6440358f04480b973128fdb47acf5c23d98a3
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-02-15 14:43:00 +0530

    Update Tree2_SymmetricBinaryTree.java

--- Tree2_SymmetricBinaryTree.java
+++ Tree2_SymmetricBinaryTree.java
@@ -14,12 +14,16 @@
 public class Solution {
     boolean isSame = true;
     public int isSymmetric(TreeNode A) {
+        //root is null
         if(A == null)
             return 1;
+        // Both left and right are null--return true
         if(A.left == null && A.right == null)
             return 1;
+        // if either of left or right is null -- return false
         if(A.left == null || A.right == null)
             return 0;
+        // if value of left = right -- return true
         if(A.left.val == A.right.val)
             return 1;
 
