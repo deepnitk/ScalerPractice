adb0df4ab74743e35da91f5451be56a89164e80d
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-22 14:26:18 +0530

    Update EditDistance.java
    Tabulation Approach

--- DP3/EditDistance.java
+++ DP3/EditDistance.java
@@ -23,3 +23,37 @@ public class Solution {
                 Math.min(minOp(A,B, i, j-1, dp), minOp(A,B, i-1, j-1, dp)));
     }
 }
+
+//Tabulation
+
+public class Solution {
+    public int minDistance(String A, String B) {
+        int n = A.length();
+        int m = B.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int[] arr : dp)
+            Arrays.fill(arr, 0);
+        return minOp(A,B, n, m, dp);
+    }
+    
+    private int minOp(String A, String B, int n, int m, int[][] dp){
+        for(int i=1;i<=n;i++){
+                dp[i][0] = i;
+        }
+        for(int j=1;j<=m;j++){
+            dp[0][j] = j; 
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(A.charAt(i-1) == B.charAt(j-1))
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = 1 + Math.min(
+                        dp[i-1][j], 
+                        Math.min(dp[i][j-1], dp[i-1][j-1]));
+            }
+        }
+        return dp[n][m];    
+    }
+}
+
