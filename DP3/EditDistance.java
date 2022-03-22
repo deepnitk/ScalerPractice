dc58f4db1d7b81b47379cffbaeed2ae5a1bb5d38
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-22 09:46:57 +0530

    Create EditDistance.java
    Given two strings A and B, find the minimum number of steps required to convert A to B. (each operation is counted as 1 step.)
    
    You have the following 3 operations permitted on a word:
    
    Insert a character
    Delete a character
    Replace a character

---
+++ DP3/EditDistance.java
@@ -0,0 +1,24 @@
+// Memoization
+public class Solution {
+    public int minDistance(String A, String B) {
+        int n = A.length();
+        int m = B.length();
+        int[][] dp = new int[n][m];
+        for(int[] arr : dp)
+            Arrays.fill(arr, -1);
+        return minOp(A,B, n-1, m-1, dp);
+    }
+
+    private int minOp(String A, String B, int i, int j, int[][] dp){
+        if(i < 0 && j < 0) return 0;
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+        
+        if(A.charAt(i) == B.charAt(j))
+            return dp[i][j] = minOp(A,B, i-1, j-1, dp);
+        else
+            return dp[i][j] = 1 + Math.min(
+                minOp(A,B, i-1, j, dp), 
+                Math.min(minOp(A,B, i, j-1, dp), minOp(A,B, i-1, j-1, dp)));
+    }
+}
