8d8193e9851c7cd312a60bc47c3650de422d6e2c
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-24 19:49:10 +0530

    Update Knapsack0_1.java

--- DP4/Knapsack0_1.java
+++ DP4/Knapsack0_1.java
@@ -37,3 +37,36 @@ public class Solution {
         return dp[idx][capacity] = Math.max(pick, notPick);
     }
 }
+
+//Tabulation
+//TC:O(N*W)
+//SC: O(N*W) + O(N)
+public class Solution {
+    public int solve(int[] values, int[] weights, int capacity) {
+        
+        return f(values.length, capacity, weights, values);
+    }
+
+    private int f(int n, int capacity, int[] weights, int[] values){
+        int[][] dp = new int[values.length+1][capacity+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i], 0);
+        }
+        //Base cond
+        for(int i=weights[0]; i<=capacity; i++){
+            dp[0][i] = values[0];
+        }
+
+       
+       for(int idx =1; idx<n; idx++){
+        for(int cap=0; cap<=capacity; cap++){
+            int pick = 0;
+            if(cap-weights[idx] >= 0)
+                pick = values[idx] + dp[idx-1][cap - weights[idx]];
+            int notPick = 0 + dp[idx-1][cap];
+            dp[idx][cap] = Math.max(pick, notPick);
+        }
+       }
+       return dp[n-1][capacity];
+    }
+}
