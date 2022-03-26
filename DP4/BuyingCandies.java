638ce3417a8b27dc89aca385c9b1ae05b0f40e0f
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-26 08:48:39 +0530

    Update BuyingCandies.java

--- DP4/BuyingCandies.java
+++ DP4/BuyingCandies.java
@@ -2,8 +2,8 @@
 
 public class Solution {
     public int solve(int[] candies, int[] sweetness, int[] cost, int nibbles) {
-        int n = candies.length-1;
-        return candiesUtils(n, candies, sweetness, cost, nibbles);
+        int n = candies.length;
+        return candiesUtils(n-1, candies, sweetness, cost, nibbles);
     }
 
     private int candiesUtils(int n, int[] candies, int[] sweetness, int[] cost, int nibbles){
@@ -18,3 +18,30 @@ public class Solution {
         return Math.max(pick, notPick);
     }
 }
+
+//Memoization solution using dp array
+
+public class Solution {
+    public int solve(int[] candies, int[] sweetness, int[] cost, int nibbles) {
+        int n = candies.length;
+        int[][] dp = new int[n+1][nibbles+1];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i], -1);
+        }
+        return candiesUtils(n-1, candies, sweetness, cost, nibbles, dp);
+    }
+
+    private int candiesUtils(int n, int[] candies, int[] sweetness, int[] cost, int nibbles, int[][] dp){
+        if(n<0 || nibbles< 0) return 0;
+        int pick = 0;
+        int notPick = 0;
+        if(dp[n][nibbles] != -1) return dp[n][nibbles];
+        if(nibbles >= cost[n])
+            pick = candies[n]*sweetness[n] + candiesUtils(n, candies, sweetness, cost, nibbles-cost[n], dp);
+        notPick = 0 + candiesUtils(n-1, candies, sweetness, cost, nibbles, dp);
+
+        return dp[n][nibbles] = Math.max(pick, notPick);
+    }
+}
+
+
