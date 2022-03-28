66cc9869445877a7110777bbaa970ce4ed7e28d4
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-03-28 08:06:29 +0530

    Create MinPathSum.java
    Given a M x N grid A of integers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
    
    Return the minimum sum of the path.
    
    NOTE: You can only move either down or right at any point in time.

---
+++ DP2/MinPathSum.java
@@ -0,0 +1,25 @@
+//Memoization solution
+
+public class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dp = new int[m][n];
+        
+        for(int row[]: dp)
+            Arrays.fill(row,-1);
+        
+        return minPathSumUtils(m-1, n-1, grid, dp);        
+    }
+
+    private int minPathSumUtils(int i, int j, int[][] grid, int[][] dp){
+        if(i==0 && j==0) return grid[0][0];
+        if(i<0 || j<0) return (int)Math.pow(10,9);
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        int up = grid[i][j] + minPathSumUtils(i-1, j, grid, dp);
+        int left = grid[i][j] + minPathSumUtils(i, j-1, grid, dp);
+        
+        return dp[i][j] = Math.min(up, left);
+    }
+}
