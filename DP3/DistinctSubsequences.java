d0859ce1cfc2703ae8a783a22b2cbe33bf7044dd
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-14 10:57:41 +0530

    Update DistinctSubsequences.java

--- DP3/DistinctSubsequences.java
+++ DP3/DistinctSubsequences.java
@@ -45,3 +45,37 @@ class Solution {
             return dp[i][j] = numDistinctUtils(i-1, j, s1, s2, dp);
     }
 }
+
+//Tabulation solution
+//TC: O(N*M)
+//SC: O(N*M)
+class Solution {
+    static int prime =(int)(Math.pow(10,9)+7);
+    public int numDistinct(String s, String t) {
+        
+        return numDistinctUtils(s, t);
+    }
+    
+    private int numDistinctUtils(String s1, String s2){
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n+1][m+1];
+        for(int[] row:dp)
+            Arrays.fill(row, 0);
+        
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 1;
+        for(int j=1;j<=m;j++)
+            dp[0][j] = 0;
+        for(int i =1; i<=n;i++){
+            for(int j=1;j<=m;j++){
+                
+                if(s1.charAt(i-1) == s2.charAt(j-1))
+                    dp[i][j] = (dp[i-1][j-1] + dp[i-1][j])%prime;
+                else
+                    dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
+    }
+}
