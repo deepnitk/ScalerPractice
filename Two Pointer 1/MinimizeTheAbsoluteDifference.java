418562448329b5d88779ac8f2764950fbffc4bb0
ratnadeep konar <ratnadeep.konar@gmail.com>
2022-04-16 18:28:23 +0530

    Update MinimizeTheAbsoluteDifference.java

--- Two Pointer 1/MinimizeTheAbsoluteDifference.java
+++ Two Pointer 1/MinimizeTheAbsoluteDifference.java
@@ -14,3 +14,30 @@ public class Solution {
         return res;
     }
 }
+
+//Optimized solution
+
+public class Solution {
+    public int solve(int[] A, int[] B, int[] C) {
+        int i = A.length -1;
+        int j = B.length -1;
+        int k = C.length -1;
+
+        int minDiff, currentDiff, maxTerm;
+        minDiff = Math.abs(Math.max(A[i], Math.max(B[j], C[k]))
+                - Math.min(A[i], Math.min(B[j], C[k])));
+        while(i != -1 && j != -1 && k != -1){
+            currentDiff = Math.abs(Math.max(A[i], Math.max(B[j], C[k]))
+                - Math.min(A[i], Math.min(B[j], C[k])));
+            
+            if(currentDiff < minDiff)
+                minDiff = currentDiff;
+            maxTerm = Math.max(A[i], Math.max(B[j] , C[k]));
+            if(maxTerm == A[i]) i--;
+            else if(maxTerm == B[j]) j--;
+            else k--;
+        }
+        return minDiff;
+    }
+}
+
