8931cea8819432dc68e807bda27b51373af8d743
ratnadek <ratnadeep.konar@amdocs.com>
2022-07-06 10:12:00 +0530

    Finish Intro to OOP class

---
+++ src/introtooop/Student.java
@@ -0,0 +1,23 @@
+package introtooop;
+
+public class Student {
+    String name;
+    String address;
+    String email;
+    String batch;
+    double psp;
+    String state;
+
+    void changeBatch(String newBatch) {
+        this.batch = newBatch;
+    }
+
+    void pauseCourse() {
+        this.state = "PAUSED";
+    }
+
+    void Nandini() {
+        System.out.println("Nandini");
+    }
+
+}
