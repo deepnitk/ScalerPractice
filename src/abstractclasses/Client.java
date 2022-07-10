0d0d45af8b920f69845480259ac5edb80035b270
ratnadek <ratnadeep.konar@amdocs.com>
2022-07-10 19:45:55 +0530

    Solid principles 1

---
+++ src/abstractclasses/Client.java
@@ -0,0 +1,32 @@
+package abstractclasses;
+
+public class Client {
+    public static void main(String[] args) {
+        Instructor instructor = new Instructor();
+        // instructor.name = "deep";
+        instructor.setName("deep");
+        // instructor.email = "deep@gmail.com";
+        instructor.setEmail("deep@gmail.com");
+        // instructor.assignedBatch = "june22";
+        instructor.setAssignedBatch("june22");
+        // instructor.password = "sonu";
+        instructor.setPassword("sonu");
+        // instructor.topic = "lld";
+        instructor.setTopic("lld");
+        // instructor.description = "super cool";
+        instructor.setDescription("super cool");
+
+        // User user = new User(); // we cannot create an object of User as it is an abstarct class
+        // user.name = "Nandini";
+        // user.setName("Nandini");
+
+        Mentor mentor = new Mentor();
+        //mentor.name = "Chandan";
+        mentor.setName("Chandan");
+        // mentor.description = "Boss";
+        mentor.setDescription("Boss");
+        // mentor.email = "chandan@gmail.com";
+        mentor.setEmail("chandan@gmail.com");
+
+    }
+}
---
+++ src/abstractclasses/Mentor.java
@@ -0,0 +1,33 @@
+package abstractclasses;
+
+import java.util.List;
+
+public class Mentor extends User {
+    private List<Student> mentees;
+    private String description;
+
+    Mentor() {
+        super("Mentor");
+    }
+
+    public List<Student> getMentees() {
+        return mentees;
+    }
+
+    public void setMentees(List<Student> mentees) {
+        this.mentees = mentees;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    void saySomething() {
+        System.out.println("reschedule 1*1.");
+    }
+}
